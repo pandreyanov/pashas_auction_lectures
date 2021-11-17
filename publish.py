5bbd8c3bf13eb8d156f9e45c0333617843b5429a
pandreyanov <pandreyanov@gmail.com>
2021-11-17 22:01:53 +0300

    Deployed ec49ea7 with MkDocs version: 1.2.3

--- publish.py
+++ publish.py
@@ -2,11 +2,21 @@ import pandas as pd
 import numpy as np
 import seaborn as sb
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 options_scatter = {'s':15, 'color':'white', 'edgecolor':'black'}
 options_hist = {'color':'white', 'bins':25, 'edgecolor':'black'}
 
-def publish(fig):
-    fig.set_size_inches(5, 6)
-    fig.set_dpi(150)
-    plt.tight_layout()
+plt.rcParams.update({
+    "font.family": "serif",
+    "font.serif": ["Palatino"],
+})
+
+def publish(entry):
+    if type(entry) == mpl.figure.Figure:
+        entry.set_size_inches(5, 6)
+        entry.set_dpi(150)
+        plt.tight_layout()
+        
+    if type(entry) == pd.core.frame.DataFrame:
+        print(entry.to_markdown(tablefmt="grid"))
